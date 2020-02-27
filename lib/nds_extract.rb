607fe7d5f979d23d06278ebf57e6abe7ae6956c3
Sara Khandaker <kttsara@hotmail.com>
2020-02-27 22:46:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,10 +7,11 @@ require 'directors_database'
 def gross_for_director(director_data)
   total=0
   rownum=0
-  while rownum< director_data[:movies].length
-  total+=director_data[:movies][rownum][:worldwide_gross]
-  rownum+=1
-end
+  binding pry
+  #while rownum< director_data[:movies].length
+  #total+=director_data[:movies][rownum][:worldwide_gross]
+  #rownum+=1
+#end
 return total
 end
 
