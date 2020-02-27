e2482fa59eebc6c4d72574fd7d63183b24cc7fe4
Sara Khandaker <kttsara@hotmail.com>
2020-02-27 22:58:25 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,14 +1,14 @@
+require 'pry'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
-# Find a way to accumulate the :worldwide_grosses and return that Integer
-# using director_data as input
+
+# Find a way to accumulate the :worldwide_grosses and return that Integer using director_data as input
 def gross_for_director(director_data)
-  nds=directors_database
   total=0
   rownum=0
-  while rownum < nds[director_data][:movies].length
-  total+=nds[director_data][:movies][rownum][:worldwide_gross]
+  while rownum< director_data[:movies].length
+  total+=director_data[:movies][rownum][:worldwide_gross]
   rownum+=1
 end
 return total
@@ -22,7 +22,7 @@ def directors_totals(nds)
     hash={}
     row=0
   while row < nds.length
-  hash[nds[row][:name]]=gross_for_director(row)
+  hash[nds[row][:name]]=gross_for_director(nds[row])
   row+=1
   end
   result = hash
